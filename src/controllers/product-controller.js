804216a5aa9c750525bcc22db7af1079fad531a5
leandronegreiros <leandronegreios07@gmail.com>
2019-12-26 09:50:08 -0400

    listando pela slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -6,7 +6,8 @@ const Product = mongoose.model('Product');
 exports.get = (req, res, next) => {
     Product
         .find({
-            active: true
+            slug: req.params.slug,
+            active: true,
         }, 'title price slug')
         .then(data => {
             res.status(200).send(data);
@@ -15,6 +16,17 @@ exports.get = (req, res, next) => {
         });
 }
 
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
 
 exports.post = (req, res, next) => {
     // res.status(200).send(req.body);
