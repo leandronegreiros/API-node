2fe3d5b0f7f6be2d9c7f42644ed84f7598a5d092
leandronegreiros <leandronegreios07@gmail.com>
2019-12-26 14:31:56 -0400

    Implementando validacoes

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator')
 
 exports.get = (req, res, next) => {
     Product
@@ -52,7 +53,17 @@ exports.getByTag = (req, res, next) => {
 }
 
 exports.post = (req, res, next) => {
-    // res.status(200).send(req.body);
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'O título deve conter pelo menos 3 caracteres');
+
+    //Se os dados forem invalidos
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
     product
         .save()
